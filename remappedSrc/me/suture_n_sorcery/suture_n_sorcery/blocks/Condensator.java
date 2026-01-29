47e028153dc474e9d57894e84d31ee553522c210
dedeleledede <erikzavan@gmail.com>
2026-01-29 10:49:31 -0300

    condensator and gauze

--- remappedSrc/me/suture_n_sorcery/suture_n_sorcery/blocks/Condensator.java
+++
@@ -1,8 +0,0 @@
-package me.suture_n_sorcery.suture_n_sorcery.blocks;
-
-public static final Block CONDENSATOR = register(
-        "condensator",
-        Block::new,
-        BlockBehaviour.Properties.of().sound(SoundType.ANVIL),
-        true
-);
