2bbc13f778a53e9202f96affedb9fb3ce9d8f777
dedeleledede <erikzavan@gmail.com>
2026-01-29 10:48:57 -0300

    remove reduntant imports

--- src/main/java/me/suture_n_sorcery/suture_n_sorcery/registries/ModEffects.java
+++ src/main/java/me/suture_n_sorcery/suture_n_sorcery/registries/ModEffects.java
@@ -1,10 +1,8 @@
 package me.suture_n_sorcery.suture_n_sorcery.registries;
 
-import me.suture_n_sorcery.suture_n_sorcery.Suture_n_sorcery;
 import me.suture_n_sorcery.suture_n_sorcery.status_effects.Bleeding;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 
 public final class ModEffects {
     public static void registerEffects(){
