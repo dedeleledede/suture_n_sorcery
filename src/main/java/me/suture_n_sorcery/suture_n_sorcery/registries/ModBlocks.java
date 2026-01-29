18e5047f753b14d36fe9ea4530ab7a9048adca2f
dedeleledede <erikzavan@gmail.com>
2026-01-29 10:49:09 -0300

    add liquid block

--- src/main/java/me/suture_n_sorcery/suture_n_sorcery/registries/ModBlocks.java
+++ src/main/java/me/suture_n_sorcery/suture_n_sorcery/registries/ModBlocks.java
@@ -1,12 +1,14 @@
 package me.suture_n_sorcery.suture_n_sorcery.registries;
 
 import me.suture_n_sorcery.suture_n_sorcery.blocks.Condensator.Condensator;
+import me.suture_n_sorcery.suture_n_sorcery.fluids.ConcentratedBlood.ConcentratedBloodBlock;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class ModBlocks {
     public static void registerBlocks(){
         Registry.register(Registries.BLOCK, Condensator.CONDENSATOR_KEY, Condensator.CONDENSATOR);
+        Registry.register(Registries.BLOCK, ConcentratedBloodBlock.CONCENTRATED_BLOOD_BLOCK_ID, ConcentratedBloodBlock.CONCENTRATED_BLOOD_BLOCK);
     }
 }
 
